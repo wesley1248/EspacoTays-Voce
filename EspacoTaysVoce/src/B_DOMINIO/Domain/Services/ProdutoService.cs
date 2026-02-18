238d41fafed62111e9b7dbe523e6d83788b5b245
wesley1248 <wesleytms.es@gmail.com>
2026-02-18 20:41:51 -0300

    Criando o service de Produto e criando o mapping do mesmo.

---
+++ EspacoTaysVoce/src/B_DOMINIO/Domain/Services/ProdutoService.cs
@@ -0,0 +1,33 @@
+using System;
+using Domain.Interfaces.Services;
+using Domain.Entities;
+
+namespace Domain.Services;
+
+public class ProdutoService : IProduto
+{
+    public async Task NewProduto(Produto produto)
+    {
+
+    }
+
+    public async Task ConsultarProduto(string id)
+    {
+
+    }
+
+    public async Task ObterTodosPaginado()
+    {
+
+    }
+
+    public async Task DeleteProduto(string id)
+    {
+
+    }
+
+    public async Task EditProduto(Produto produto)
+    {
+
+    }
+}
